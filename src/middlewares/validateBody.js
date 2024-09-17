a103970d8d5d0e548b88e494c0a0eda8e84e237d
Yuri <wall.mart.boss@proton.me>
2024-09-17 17:06:37 +0300

    upd

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -13,13 +13,9 @@ export const validateBody = (schema) => async (req, res, next) => {
       type: detail.type,
     }));
 
-    const error = createError(
-      errorDetails.detail.type,
-      errorDetails.detail.message,
-      {
-        errors: errorDetails,
-      },
-    );
+    const error = createError(400, 'Validation Error', {
+      errors: errorDetails,
+    });
 
     if (err.code) {
       console.error(`Error code: ${err.code}`);
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -7,29 +7,36 @@ export const createContactSchema = Joi.object({
     'string.max': 'Username should have at most {#limit} characters',
     'any.required': 'Username is required',
   }),
-  phoneNumber: Joi.string().min(3).max(20).required().messages({
-    'string.base': 'PhoneNumber can consist of only numbers, signs +, (, ) or -', // Кастомізація повідомлення для типу "string"
-    'string.min': 'PhoneNumber name should have at least {#limit} characters',
-    'string.max': 'PhoneNumber should have at most {#limit} characters',
-    'any.required': 'phoneNumber is required',
-  }),
+  phoneNumber: Joi.string()
+    .pattern(/^[0-9\+\-\(\)\s]+$/)
+    .min(3)
+    .max(20)
+    .required()
+    .messages({
+      'string.pattern.base':
+        'PhoneNumber can consist of only numbers, signs +, (, ), or -',
+      'string.min': 'PhoneNumber name should have at least {#limit} characters',
+      'string.max': 'PhoneNumber should have at most {#limit} characters',
+      'any.required': 'phoneNumber is required',
+    }),
   email: Joi.string().email(),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
 });
 
 export const updateContactSchema = Joi.object({
-    name: Joi.string().min(3).max(20).messages({
-        'string.base': 'Username should be a string', // Кастомізація повідомлення для типу "string"
-        'string.min': 'Username should have at least {#limit} characters',
-        'string.max': 'Username should have at most {#limit} characters',
-    }),
-    phoneNumber: Joi.string().min(3).max(20).messages({
-        'string.base': 'PhoneNumber can consist of only numbers, signs +, (, ) or -', // Кастомізація повідомлення для типу "string"
-        'string.min': 'PhoneNumber name should have at least {#limit} characters',
-        'string.max': 'PhoneNumber should have at most {#limit} characters',
-    }),
-    email: Joi.string().email(),
-    isFavourite: Joi.boolean(),
-    contactType: Joi.string().valid('work', 'home', 'personal'),
+  name: Joi.string().min(3).max(20).messages({
+    'string.base': 'Username should be a string', // Кастомізація повідомлення для типу "string"
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+  }),
+  phoneNumber: Joi.string().min(3).max(20).messages({
+    'string.base':
+      'PhoneNumber can consist of only numbers, signs +, (, ) or -', // Кастомізація повідомлення для типу "string"
+    'string.min': 'PhoneNumber name should have at least {#limit} characters',
+    'string.max': 'PhoneNumber should have at most {#limit} characters',
+  }),
+  email: Joi.string().email(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
 });

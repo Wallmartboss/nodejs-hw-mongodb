ae922988bca9fc27b4ff8094e35775366cab0f3d
Yuri <wall.mart.boss@proton.me>
2024-09-17 20:43:23 +0300

    Update contacts.js

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -13,7 +13,7 @@ const router = Router();
 
 router.get('/contacts', ctrlWrapper(getAllContactsController));
 router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
-router.post('/', ctrlWrapper(createContactController));
+router.post('/contacts', ctrlWrapper(createContactController));
 router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 router.put('/contacts/:contactId', ctrlWrapper(upsertContactController));
 router.patch('/contacts/:contactId', ctrlWrapper(patchContactController));

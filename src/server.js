de27e1ba5f193763b746d82d2f778528a1aa6d04
Yuri <wall.mart.boss@proton.me>
2024-09-23 23:35:56 +0300

    final

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import router from './routers/index.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -17,7 +18,7 @@ export const setupServer = () => {
   app.use(cors());
   app.use(cookieParser());
   app.use('/uploads', express.static(UPLOAD_DIR));
-
+  app.use('/api-docs', swaggerDocs());
   app.use(
     pino({
       transport: {
@@ -25,13 +26,9 @@ export const setupServer = () => {
       },
     }),
   );
-
   app.use(router);
-
   app.use('*', notFoundHandler);
-
   app.use(errorHandler);
-
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
